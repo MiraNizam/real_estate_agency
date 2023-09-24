6fcfac8ab0cc1b58bec48894251ca44cb85f8b40
MiraNizam <82273561+MiraNizam@users.noreply.github.com>
2023-09-24 19:27:05 +0300

    Update 0004_auto_20230917_2116.py

--- property/migrations/0004_auto_20230917_2116.py
+++ property/migrations/0004_auto_20230917_2116.py
@@ -5,8 +5,8 @@ from django.db import migrations
 
 def is_new_building(apps, schema_editor):
     Flat = apps.get_model('property', 'Flat')
-    Flat.objects.filter(construction_year_lt=2015).update(new_building=False)
-    Flat.objects.filter(construction_year_gte=2015).update(new_building=True)
+    Flat.objects.filter(construction_year__lt=2015).update(new_building=False)
+    Flat.objects.filter(construction_year__gte=2015).update(new_building=True)
 
 
 class Migration(migrations.Migration):
