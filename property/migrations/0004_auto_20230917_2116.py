c9a14e667f322ec760dff21a4c37af553e0bba07
MiraNizam <82273561+MiraNizam@users.noreply.github.com>
2023-09-24 19:03:49 +0300

    Reduce number of queries to db

--- property/migrations/0004_auto_20230917_2116.py
+++ property/migrations/0004_auto_20230917_2116.py
@@ -5,12 +5,8 @@ from django.db import migrations
 
 def is_new_building(apps, schema_editor):
     Flat = apps.get_model('property', 'Flat')
-    for flat in Flat.objects.all():
-        if flat.construction_year < 2015:
-            flat.new_building = False
-        else:
-            flat.new_building = True
-        flat.save()
+    Flat.objects.filter(construction_year_lt=2015).update(new_building=False)
+    Flat.objects.filter(construction_year_gte=2015).update(new_building=True)
 
 
 class Migration(migrations.Migration):
